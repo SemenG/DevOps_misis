1732a91230ac4f853673ccf4fda8a615c392a90b
SemenG <semen.grischin2015@gmail.com>
2026-05-20 01:07:21 +0300

    fix for flake8

--- app/api/main.py
+++ app/api/main.py
@@ -15,24 +15,18 @@ recommender.fit(df_train)
 @app.get("/", response_class=HTMLResponse)
 def get_simple_form(user_id: int = None, top_n: int = 5):
     result_html = ""
-
     if user_id is not None:
         recs = get_top_recommendations(user_id=user_id, model=recommender, top_n=top_n)
-
-
         list_items = ""
         for item_id in recs:
             book_text = loader.get_book_info(item_id)
             list_items += f"<li>{book_text}</li>"
-
         result_html = f"""
         <h3>Рекомендации для Пользователя {user_id}:</h3>
         <ol>
             {list_items}
         </ol>
         """
-
-
     html_content = f"""
     <html>
         <head>
@@ -43,10 +37,8 @@ def get_simple_form(user_id: int = None, top_n: int = 5):
             <form method="get" action="/">
                 <label>ID Пользователя: </label>
                 <input type="number" name="user_id" value="{user_id if user_id else 1}" required><br><br>
-
                 <label>Количество рекомендаций: </label>
                 <input type="number" name="top_n" value="{top_n}" min="1" max="20" required><br><br>
-
                 <button type="submit">Получить</button>
             </form>
             <hr>
